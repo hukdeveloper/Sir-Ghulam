9d7ca50e07e4b31869be0107d3a316836d4443a6
GH NAGRI <103577768+Hussain-Ghulam@users.noreply.github.com>
2023-10-16 14:01:57 +0100

    Update Dialog.js

--- src/data/Dialog.js
+++ src/data/Dialog.js
@@ -1662,10 +1662,14 @@ export const dialoguesData = [
         Therapist:
           "The guidelines recommend that adults get a minimum of 150 to 300 minutes of moderate-intensity aerobic physical activity per week or 75 minutes to 150 minutes a week of vigorous-intensity aerobic physical activity.",
         Client: "Wow, I think I only get about an hour right now.",
+      },
+      {
         Client: "I try to walk once or twice per week, for about 30 minutes.",
         Therapist:
           "Well that's a great start, it sounds like you already found an activity you enjoy.",
         Therapist: "Maybe you can work on increasing the time spent jogging?",
+      },
+      {
         Client: "Yeah, I think I can do that.",
       },
     ],
